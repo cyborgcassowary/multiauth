4ef57f9e3833941f5ac2e64d594d99e437009939
Ryan Hettler <ryan@ryanhettler.com>
2014-12-17 15:49:57 -0500

    Multi Auth with Laravel - part 1
    Trying to accomplish multi-user tables in Laravel.

---
+++ app/config/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	'multi' => array(
+		'account' => array(
+			'driver' => 'eloquent',
+			'model' => 'Account'
+		),
+		'user' => array(
+			'driver' => 'database',
+			'table' => 'users'
+		)
+	),
+
+	'reminder' => array(
+
+		'email' => 'emails.auth.reminder',
+
+		'table' => 'password_reminders',
+
+		'expire' => 60,
+
+	),
+
+);
---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+//use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+			User::create([
+				'username' => 'ryanhettler',
+				'email' => 'ryan@ryanhettler.com',
+				'password' => Hash::make('WhatDoYouGoHomeTo?')
+			]);
+			User::create([
+				'username' => 'cyborgcassowary',
+				'email' => 'info@cyborgcassowary.com',
+				'password' => Hash::make('ItIsNatural2BeAfraid')
+			]);
+		// Passwords titled after Explosions in the Sky songs on 'All Of A Sudden I Miss Everyone':
+		// https://www.youtube.com/watch?v=6sDtb5-JrqE
+	}
+
+}
